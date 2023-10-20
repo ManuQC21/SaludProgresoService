90ec9572a74bd8bd57a9808c9856bbfc54b675f0
ManuQC21 <mquispecueva1@upao.edu.pe>
2023-10-20 00:21:06 -0500

    Revert "Implementacion de Agendar y Listar Cita medica"
    This reverts commit 4f0fc93be30b45da2c05ae527341ce5306f58c33, reversing
    changes made to e48b7ea83a5e43174c2ee30f0b8a339b5e223bca.

--- src/main/java/com/upao/entity/CitasMedicas.java
+++ src/main/java/com/upao/entity/CitasMedicas.java
@@ -19,7 +19,6 @@ public class CitasMedicas {
 
     private LocalDateTime fechaHoraCita;
 
-
     @Column(length = 255)
     private String areaEspecialidad;
 
@@ -85,6 +84,4 @@ public class CitasMedicas {
     public void setRecordatorioCita(boolean recordatorioCita) {
         this.recordatorioCita = recordatorioCita;
     }
-
-
 }
